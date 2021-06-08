55bacf581a399aecee663c47285f1ee0225cf007
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-06-08 09:03:05 -0500

    working on app.js

---
+++ resources/js/app.js
@@ -0,0 +1,19 @@
+//  SELECT ALL ELEMENTS
+const country_name_element = document.querySelector(".country .name");
+const total_cases_element = document.querySelector(".total-cases .value");
+const new_cases_element = document.querySelector(".total-cases .new-value");
+const recovered_element = document.querySelector(".recovered .value");
+const new_recovered_element = document.querySelector(".recovered .new-value");
+const deaths_element = document.querySelector(".deaths .value");
+const new_deaths_element = document.querySelector(".deaths .new-value");
+
+const ctx = document.getElementById("axes_line_chart").getContext("2d");
+
+// APP VARIABLES
+let app_data = [],
+  cases_list = [],
+  recovered_list = [],
+  deaths_list = [],
+  dates = [];
+
+// GET USERS COUNTRY CODE
