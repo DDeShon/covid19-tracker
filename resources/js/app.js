1c9a16f2f120526a8fd5344eef2e25bb847627cf
DDeShon <36036284+DDeShon@users.noreply.github.com>
2021-06-09 09:41:13 -0500

    added security policy

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,7 +18,7 @@ let app_data = [],
   formattedDates = [];
 
 // GET USERS COUNTRY CODE
-let country_code = geoplugin_countryCode("US");
+let country_code = geoplugin_countryCode();
 let user_country;
 country_list.forEach((country) => {
   if (country.code == country_code) {
